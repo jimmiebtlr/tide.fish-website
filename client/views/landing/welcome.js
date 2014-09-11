230f0a4bb332e9ecffb2e52452edfe85bc13b96a
Jimmie Butler <jimmiebtlr@gmail.com>
2014-09-11 02:59:51 -0500

    Fix welcome footer hide.

--- client/views/landing/welcome.js
+++ client/views/landing/welcome.js
@@ -1,8 +1,10 @@
 Template.welcome.created = function(){
   GAnalytics.pageview();
   Session.set('hideNav', true);
+  Session.set('hideFooter', true);
 }
 
 Template.welcome.destroyed = function(){
   Session.set('hideNav', undefined);
+  Session.set('hideFooter', undefined);
 }
