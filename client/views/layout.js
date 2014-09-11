c41c259a7cd4399fa3303634315b6882b398fdf2
Jimmie Butler <jimmiebtlr@gmail.com>
2014-09-11 14:54:27 -0500

    Rework css on welcome to be better at responsive.

--- client/views/layout.js
+++ client/views/layout.js
@@ -6,7 +6,15 @@ Template.layout.hideFooter = function(){
   return Session.get('hideFooter');
 }
 
-Template.footer.rendered = function(){ 
-  var footerHeight = $("#footer").innerHeight();
-  $("body").css("margin-bottom", footerHeight);
+Template.nav.rendered = function(){
+  if( Session.get('hideNav') === undefined ){
+    var navHeight = $("#nav").innerHeight();
+    $("body").css("margin-top", navHeight);
+  }
+}
+Template.footer.rendered = function(){
+  if( Session.get('hideFooter') === undefined ){
+    var footerHeight = $("#footer").innerHeight();
+    $("body").css("margin-bottom", footerHeight);
+  }
 }
