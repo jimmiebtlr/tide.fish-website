ef6dbd9a666caa7a1c297102b5b1ca2470293103
Jimmie Butler <jimmiebtlr@gmail.com>
2014-09-12 23:25:08 -0500

    Fix register button labels on welcome page.

--- client/views/landing/welcome.js
+++ client/views/landing/welcome.js
@@ -2,9 +2,11 @@ Template.welcome.created = function(){
   GAnalytics.pageview();
   Session.set('hideNav', true);
   Session.set('hideFooter', true);
+  Session.set('buttonText', "up");
 }
 
 Template.welcome.destroyed = function(){
   Session.set('hideNav', undefined);
   Session.set('hideFooter', undefined);
+  Session.set('buttonText', undefined);
 }
