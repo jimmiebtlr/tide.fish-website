e2af82ea3d42f6e1560350fc8e9434d2f721b92c
Jimmie Butler <jimmiebtlr@gmail.com>
2014-09-19 14:37:40 -0500

    Add velocity.

---
+++ tests/jasmine/client/integration/boat.js
@@ -0,0 +1,45 @@
+describe("Boat", function(){
+  beforeEach( function(){
+    MeteorStubs.install();
+    mock(window, 'Accounts');
+    valid = {name: "test"};
+  });
+
+  afterEach(function(){
+    MeteorStubs.uninstall();
+    valid = undefined;
+  });
+
+  describe("insert", function(){
+    it("should be allowed for a valid boat",function(){
+      Meteor.loginWithPassword('test@test.com', 'test',function(){
+        var context = BoatSchema.newContext();
+        var validClean = BoatSchema.clean(valid);
+        context.validate(validClean);
+        expect( context.invalidKeys() ).toBe(true);
+        check(null, String);
+        done();
+      });
+    });
+    it("should not be allowed for a boat without a name",function(){
+      spyOn(Meteor, "userId").and.returnValue(Random.id());
+      expect( Boats.insert({}) ).toThrow();
+    });
+  });
+  describe("update", function(){
+    beforeEach( function(){
+      Boats.insert({name: "Test"});
+    });
+    it("should fail when changing ownerId",function(){
+      check( {name: "test"}, BoatSchema );
+      expect(false).toBeFalsy();
+    });
+    //it("should not allow another user to change it");
+    //it("should allow a user to remove themself from allowedBookingUsers");
+  });
+  describe("remove", function(){
+    beforeEach( function(){
+      Boats.insert({name: "Test"});
+    });
+  });
+});
