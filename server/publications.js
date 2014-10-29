b78e34ce4068dfc92ebb2506a44743cdbdd6817a
Jimmie Butler <jimmiebtlr@gmail.com>
2014-10-28 20:39:58 -0500

    Add basic laika tests for schedule.

--- server/publications.js
+++ server/publications.js
@@ -28,38 +28,70 @@ Meteor.publish('User', function(){
 });
 
 Meteor.publish('PublicSchedule',function(id, startDate, endDate){
+  var self = this;
   check( id, String );
   check( startDate, String );
   check( endDate, String );
-
+  
   var start = moment(startDate);
   var end = moment(endDate );
 
   check( start.diff(end, 'months'), Match.Where( function( diff ){
     return diff <= 1;
   }));
-
-  var publicSchedule = [];
-
+  
+  var initializing = true;
+  var collectionName = "Schedule";
+  
   var boatIds = Boats.relatedPublishedIds( id );
-
   var mapTripLength = function(a){
     return TripLengths.findOne( a ).label;
   };
+  var calcId = function( date, boatId ){
+    return boatId + date.format("MMDDYYYY");
+  }
+
+
+  var handle = Bookings.find({'boatId': {$in: boatIds}}).observeChanges({
+    added: function ( id, doc) {
+      if( !initializing ){
+        self.changed( collectionName, calcId(date,boatId), rec );
+      }
+    },
+    changed: function( id, doc){
+      if( !initializing ){
+        console.log( Bookings.findOne(id) );
+        console.log( doc );
+        // send change for both old date and new date if not the same
+        var avaliable = _.map(Bookings.avaliable(date,boatId), mapTripLength);
+        self.changed( collectionName, calcId(date,boatId), rec );
+      }
+    },
+    removed: function( id ){
+      if( !initializing ){
+        self.changed( collectionName, calcId(date,boatId), rec );
+      }
+    }
+  });
 
   _.each( boatIds, function( boatId ){
     var boatName = Boats.findOne( boatId ).name;
     for( var date = start.clone(); !date.isAfter(end); date.add(1,'days') ){
-      console.log( date.format("MM/DD"));
       var avaliable = _.map(Bookings.avaliable(date,boatId), mapTripLength);
       var schedule = {
+        boatId: boatId,
         boatName: boatName,
-        date: date.clone(),
+        date: date.toDate(),
         avaliable: avaliable
       };
-      publicSchedule.push( schedule );
+      self.added(collectionName, calcId(date,boatId), schedule );
     }
   });
 
-  return publicSchedule;
+  initializing = false;
+  self.ready();
+  
+  self.onStop(function () {
+    handle.stop();
+  });
 });
